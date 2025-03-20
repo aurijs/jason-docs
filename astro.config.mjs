72ce8534c7b56dc91f7ee20a0e869ffdbf21a503
realfakenerd <lucas210898@hotmail.com>
2025-03-20 17:55:48 -0300

    feat(docs): update documentation and assets for JasonDB
    - Replace outdated assets with new logo and hero images
    - Add new documentation pages including "Getting Started", "How It Works", and "The JasonDB Class"
    - Update package.json with new dependencies for TailwindCSS, Vercel, and compression
    - Modify astro.config.mjs to configure new site settings and integrations
    - Add global.css for custom styling and animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,84 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import viteCompression from "vite-plugin-compression";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://jasondb.vercel.app/",
+  integrations: [
+    starlight({
+      title: "JasonDB",
+      lastUpdated: true,
+      credits: true,
+      editLink: {
+        baseUrl: "https://github.com/aurijs/jason-docs/edit/main/docs",
+      },
+      social: {
+        github: "https://github.com/aurijs/jason",
+      },
+      favicon: "favicon.svg",
+      head: [
+        {
+          tag: "meta",
+          attrs: {
+            name: "view-transition",
+            content: "same-origin",
+          },
+        },
+      ],
+      logo: {
+        dark: "./src/assets/logo_dark.webp",
+        light: "./src/assets/logo_light.webp",
+        alt: "JasonDB logo",
+        replacesTitle: true,
+      },
+      customCss: ["./src/styles/global.css"],
+      sidebar: [
+        {
+          label: "Start Here",
+          items: ["getting-started"],
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+      ],
+    }),
+  ],
+  vite: {
+    plugins: [
+      viteCompression({
+        algorithm: "brotliCompress",
+        verbose: false,
+        threshold: 512,
+        compressionOptions: {
+          level: 3,
+        },
+      }),
+    ],
+    build: {
+      minify: "terser",
+      target: "esnext",
+      reportCompressedSize: false,
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          pure_getters: true,
+          drop_console: true,
+        },
+      },
+    },
+  },
+  adapter: vercel({
+    isr: true,
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+      domains: [""],
+    },
+  }),
 });
